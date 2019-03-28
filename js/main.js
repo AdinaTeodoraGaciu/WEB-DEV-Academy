303258bf82c27199c2609bc12d129b4ac07d8669
Adina Gaciu <teodora.gaciu@gmail.com>
2019-03-28 22:33:12 +0200

    Created login modal and event to close modal on click

--- js/main.js
+++ js/main.js
@@ -22,3 +22,54 @@ function showSlides() {
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
+// Modal script
+
+const login_btn = document.querySelector('#login_btn');
+const modal_login = document.querySelector('#modal_wrapper_login');
+const close_login = document.querySelector('#close_login');
+const Register_btn = document.querySelector('#modal_register');
+const  modal_Register = document.querySelector('#modal_wrapper_register');
+const close_register = document.querySelector('#close_register');
+
+function getTarget(event, id_name, display_style){
+    if(event.target.id===id_name){
+        modal_login.style.display = display_style;
+    }
+}
+
+
+// register_actions = [login_btn, modal_login, close_login];
+//
+// register_actions.forEach(function(action) {
+//    action.onclick = function(event){
+//     if(event.target.id==="modal_wrapper_login" || event.target.id==="close_login"){
+//         getTarget(event.target.id, event.target.id,"none");
+//     }else{
+//         getTarget(event.target.id, event.target.id,"block");
+//     }
+//    };
+// });
+
+
+login_btn.addEventListener('click', function(event){
+    getTarget(event,"login_btn","block");
+});
+
+window.onclick = function(event){
+    getTarget(event,"modal_wrapper_login","none");
+};
+
+close_login.onclick = function(event){
+    getTarget(event,"close_login","none");
+};
+Register_btn.addEventListener('click', function(event){
+    getTarget(event,"login_btn","block");
+});
+
+window.onclick = function(event){
+    getTarget(event,"modal_wrapper_register","none");
+};
+
+close_register.onclick = function(event){
+    getTarget(event,"close_register","none");
+};
