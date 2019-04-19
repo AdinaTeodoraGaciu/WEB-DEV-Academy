67fbdccdc355d97caa65bd86d07b4ba9a7708db5
Adina Gaciu <teodora.gaciu@gmail.com>
2019-04-19 10:30:02 +0300

    Modified fetch urls and refactored functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
-var slideIndex = 0;
+
+// For the responsive nav
+// Phase one on window resize
+window.onresize = ()=>{
+    let windowWidth = window.innerWidth;
+
+    for(let i = 0; i<document.querySelectorAll('ul.nav li.nav-item').length; i++){
+        if(windowWidth <= 1020) {
+            document.querySelectorAll('ul.nav li.nav-item')[0].style.display = "block";
+            document.querySelectorAll('ul.nav li.nav-item')[0].style.marginTop = "-20px";
+            document.querySelectorAll('ul.nav li.nav-item')[i].style.display = "none";
+            document.querySelector('ul.nav ul#myCart').style.display = "none";
+        }else{
+            document.querySelectorAll('ul.nav li.nav-item')[0].style.display ="none";
+            document.querySelectorAll('ul.nav li.nav-item')[i].style.display = "block";
+            document.querySelector('ul.nav ul#myCart').style.display = "block";
+        }
+    }
+};
+
+// Phase two on clicking the bars_change button
+//When we click on the bars_change button we want the nav to display either block or none depending on the state
+document.querySelectorAll('ul.nav li.nav-item')[0].addEventListener('click', ()=>{
+    for(let i = 0; i<document.querySelectorAll('ul.nav li.nav-item').length; i++){
+        if(document.querySelectorAll('ul.nav li.nav-item')[i+1].style.display === "none"){
+            document.querySelectorAll('ul.nav li.nav-item')[i+1].style.display = "block";
+            document.querySelector('ul.nav ul#myCart').style.display = "block";
+        }else{
+            document.querySelectorAll('ul.nav li.nav-item')[i+1].style.display = "none";
+            document.querySelector('ul.nav ul#myCart').style.display = "none";
+        }
+    }
+
+});
+let slideIndex = 0;
 
 showSlides();
 
 function showSlides() {
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -44,20 +78,6 @@ function getTarget(event, id_name, display_style){
     }
 }
 
-
-// register_actions = [login_btn, modal_login, close_login];
-//
-// register_actions.forEach(function(action) {
-//    action.onclick = function(event){
-//     if(event.target.id==="modal_wrapper_login" || event.target.id==="close_login"){
-//         getTarget(event.target.id, event.target.id,"none");
-//     }else{
-//         getTarget(event.target.id, event.target.id,"block");
-//     }
-//    };
-// });
-
-
 login_btn.addEventListener('click', function(event){
     getTarget(event,"login_btn","block");
 });
@@ -80,96 +100,94 @@ window.onclick = function(event){
 close_register.onclick = function(event){
     getTarget(event,"close_register","none");
 };
-const jsonData = 'js/restaurants.json';
- fetch(jsonData)
-    .then(function (res) {
-        return res.json();
+// ------------------------------------------
+//  FETCH FUNCTION
+// ------------------------------------------
+const urls = ['js/restaurants.json','js/menu.json'];
+function fetchData(){
+    Promise.all(urls.map(url =>
+        fetch(url)
+            .then(checkStatus)
+            .then(parseJSON)
+            .catch(error => console.log('There was a problem!', error))
+    ))
+        .then(data => {
+            const restaurantList = data[0];
+            const menuList = data[1];
+            console.log(menuList);
+            getRestaurantData(restaurantList);
+        })
+
+
+}
+
+function checkStatus(response) {
+    if (response.ok) {
+        return Promise.resolve(response);
+    } else {
+        return Promise.reject(new Error(response.statusText));
+    }
+}
+function parseJSON(response) {
+    return response.json();
+}
+ function getRestaurantData(data){
+    if(data){
+        displayRestaurants(data);
+    }
+ }
+
+function generateResturantCards(data){
+     let restaurantIds = [];
+    if(data) {
+        for (let i = 0; i < data.restaurants.length; i++) {
+            document.querySelector('#restaurantsContainer').innerHTML +=
+            `<div class='column-item' id='${data.restaurants[i].id}'>
+              <article class='card-item'>
+                <div class='item-content'><h4> ${data.restaurants[i].name}</h4></div>
+                    <div class='item-image'><img src='${data.restaurants[i].icon}'></div>
+                         <div class = 'item-content'><p>" ${data.restaurants[i].deal} </p> </div></article> </div>`
+            restaurantIds.push(data.restaurants[i].id);
+        }
+
+    }
+ generateMenu(restaurantIds);
+}
+function generateMenu (ids){
+    ids.forEach(function(data_btn){
+        data_btn.addEventListener('click', (event)=> {
+            switch (event.target.id) {
+                case "gusto":
+                    generateRestaurantMenu(data);
+                    break;
+                case "kfc":
+                    generateRestaurantMenu();
+                    break;
+            }
+         })
     })
-    .then (function(jsonData){
-        restaurants = jsonData;
-        /*const gusto = document.querySelector('#gusto');
-         if(gusto) {
-         gusto.onclick = () => {
-         console.log(jsonData.restaurants);
+}
 
-
-         }
-         }*/
-    })
-
-function displayRestaurants (restaurants){
+function displayRestaurants (data){
     document.querySelector('#kitchens').style.display = 'none';
     document.querySelector('#favorites').style.display = 'none';
     document.querySelector('#options').style.display = 'none';
-    if(restaurants) {
-        for (let i = 0; i < restaurants.restaurants.length; i++) {
-            document.querySelector('#restaurantsContainer').innerHTML +=
-                `<div class='column-item' id='${restaurants.restaurants[i].id}' onclick ="displayMenu(restaurants)"'>
-                            <article class='card-item'>
-                                <div class='item-content'><h4> ${restaurants.restaurants[i].name}</h4></div>
-                                    <div class='item-image'><img src='${restaurants.restaurants[i].icon}'></div>
-                                     <div class = 'item-content'><p>" ${restaurants.restaurants[i].deal} </p> </div></article> </div>`
-
-        }
-
-    }
+    generateResturantCards(data);
 
 }
+document.querySelector('#comfortFood').addEventListener('click',fetchData);
 
-function displayMenu(restaurants){
-    if(restaurants){
-      for (let i =0;  i < restaurants.restaurants.length; i++) {
-          console.log(restaurants.restaurants[i].id);
-          switch (restaurants.restaurants[i].id){
-              case 'gusto':
-                  document.querySelector('#menuContainer').innerHTML +=
-                      `<h4>${restaurants.restaurants[i].menu.Paste}</h4>`;
-                  break;
-console.log(restaurants.restaurants[i].menu)
-          }
-
+function generateRestaurantMenu() {
+    fetchData();
+    if (data) {
+        for (let i = 0; i < data.menu.length; i++) {
             document.querySelector('#menuContainer').innerHTML +=
-                `<div class='column-item' id='${restaurants.restaurants[i].id}'>
-                            <article class='card-item'>
-                                <div class='item-content'><h4> ${restaurants.restaurants[i].menu['Paste']}</h4></div>
-                                    <div class='item-image'><img src='${restaurants.restaurants[i].icon}'></div>
-                                     <div class = 'item-content'><p>" ${restaurants.restaurants[i].deal} </p> </div></article> </div>`
+                `<table> <tr>
+                   <td>${data.menu[i].nume}</td>
+                     </tr>
+                    </table>`
         }
-
-
     }
-
 }
-// For the responsive nav
-// Phase one on window resize
-window.onresize = ()=>{
-    let windowWidth = window.innerWidth;
 
-    for(let i = 0; i<document.querySelectorAll('ul.nav li.nav-item').length; i++){
-        if(windowWidth <= 1020) {
-            document.querySelectorAll('ul.nav li.nav-item')[0].style.display = "block";
-            document.querySelectorAll('ul.nav li.nav-item')[0].style.marginTop = "-20px";
-            document.querySelectorAll('ul.nav li.nav-item')[i].style.display = "none";
-            document.querySelector('ul.nav ul#myCart').style.display = "none";
-        }else{
-            document.querySelectorAll('ul.nav li.nav-item')[0].style.display ="none";
-            document.querySelectorAll('ul.nav li.nav-item')[i].style.display = "block";
-            document.querySelector('ul.nav ul#myCart').style.display = "block";
-        }
-    }
-};
 
-// Phase two on clicking the bars_change button
-//When we click on the bars_change button we want the nav to display either block or none depending on the state
-document.querySelectorAll('ul.nav li.nav-item')[0].addEventListener('click', ()=>{
-    for(let i = 0; i<document.querySelectorAll('ul.nav li.nav-item').length; i++){
-        if(document.querySelectorAll('ul.nav li.nav-item')[i+1].style.display === "none"){
-            document.querySelectorAll('ul.nav li.nav-item')[i+1].style.display = "block";
-            document.querySelector('ul.nav ul#myCart').style.display = "block";
-        }else{
-            document.querySelectorAll('ul.nav li.nav-item')[i+1].style.display = "none";
-            document.querySelector('ul.nav ul#myCart').style.display = "none";
-        }
-    }
-
-});
