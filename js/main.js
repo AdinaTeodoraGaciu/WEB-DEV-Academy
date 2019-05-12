3748a4cb31e81d8bfc9d4dda1acae76108f8d67a
Adina Gaciu <teodora.gaciu@gmail.com>
2019-05-13 00:05:01 +0300

    Created product items, add to cart with local storage

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-
 // For the responsive nav
 // Phase one on window resize
-window.onresize = ()=>{
+window.onresize = () => {
     let windowWidth = window.innerWidth;
 
-    for(let i = 0; i<document.querySelectorAll('ul.nav li.nav-item').length; i++){
-        if(windowWidth <= 1020) {
+    for (let i = 0; i < document.querySelectorAll('ul.nav li.nav-item').length; i++) {
+        if (windowWidth <= 1020) {
             document.querySelectorAll('ul.nav li.nav-item')[0].style.display = "block";
             document.querySelectorAll('ul.nav li.nav-item')[0].style.marginTop = "-20px";
             document.querySelectorAll('ul.nav li.nav-item')[i].style.display = "none";
             document.querySelector('ul.nav ul#myCart').style.display = "none";
-        }else{
-            document.querySelectorAll('ul.nav li.nav-item')[0].style.display ="none";
+        } else {
+            document.querySelectorAll('ul.nav li.nav-item')[0].style.display = "none";
             document.querySelectorAll('ul.nav li.nav-item')[i].style.display = "block";
             document.querySelector('ul.nav ul#myCart').style.display = "block";
         }
@@ -20,13 +19,13 @@ window.onresize = ()=>{
 
 // Phase two on clicking the bars_change button
 //When we click on the bars_change button we want the nav to display either block or none depending on the state
-document.querySelectorAll('ul.nav li.nav-item')[0].addEventListener('click', ()=>{
-    for(let i = 0; i<document.querySelectorAll('ul.nav li.nav-item').length; i++){
-        if(document.querySelectorAll('ul.nav li.nav-item')[i+1].style.display === "none"){
-            document.querySelectorAll('ul.nav li.nav-item')[i+1].style.display = "block";
+document.querySelectorAll('ul.nav li.nav-item')[0].addEventListener('click', () => {
+    for (let i = 0; i < document.querySelectorAll('ul.nav li.nav-item').length; i++) {
+        if (document.querySelectorAll('ul.nav li.nav-item')[i + 1].style.display === "none") {
+            document.querySelectorAll('ul.nav li.nav-item')[i + 1].style.display = "block";
             document.querySelector('ul.nav ul#myCart').style.display = "block";
-        }else{
-            document.querySelectorAll('ul.nav li.nav-item')[i+1].style.display = "none";
+        } else {
+            document.querySelectorAll('ul.nav li.nav-item')[i + 1].style.display = "none";
             document.querySelector('ul.nav ul#myCart').style.display = "none";
         }
     }
@@ -43,12 +42,14 @@ function showSlides() {
         slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
 // Thumbnail image controls
@@ -69,125 +70,139 @@ const login_btn = document.querySelector('#login_btn');
 const modal_login = document.querySelector('#modal_wrapper_login');
 const close_login = document.querySelector('#close_login');
 const Register_btn = document.querySelector('#modal_register');
-const  modal_Register = document.querySelector('#modal_wrapper_register');
+const modal_Register = document.querySelector('#modal_wrapper_register');
 const close_register = document.querySelector('#close_register');
 
-function getTarget(event, id_name, display_style){
-    if(event.target.id===id_name){
+function getTarget(event, id_name, display_style) {
+    if (event.target.id === id_name) {
         modal_login.style.display = display_style;
     }
 }
 
-login_btn.addEventListener('click', function(event){
-    getTarget(event,"login_btn","block");
+login_btn.addEventListener('click', function (event) {
+    getTarget(event, "login_btn", "block");
 });
 
-window.onclick = function(event){
-    getTarget(event,"modal_wrapper_login","none");
+window.onclick = function (event) {
+    getTarget(event, "modal_wrapper_login", "none");
 };
 
-close_login.onclick = function(event){
-    getTarget(event,"close_login","none");
+close_login.onclick = function (event) {
+    getTarget(event, "close_login", "none");
 };
-Register_btn.addEventListener('click', function(event){
-    getTarget(event,"login_btn","block");
+Register_btn.addEventListener('click', function (event) {
+    getTarget(event, "login_btn", "block");
 });
 
-window.onclick = function(event){
-    getTarget(event,"modal_wrapper_register","none");
+window.onclick = function (event) {
+    getTarget(event, "modal_wrapper_register", "none");
 };
 
-close_register.onclick = function(event){
-    getTarget(event,"close_register","none");
+close_register.onclick = function (event) {
+    getTarget(event, "close_register", "none");
 };
 // ------------------------------------------
 //  FETCH FUNCTION
 // ------------------------------------------
-const urls = ['js/restaurants.json','js/menu.json'];
-function fetchData(){
-    Promise.all(urls.map(url =>
-        fetch(url)
-            .then(checkStatus)
-            .then(parseJSON)
-            .catch(error => console.log('There was a problem!', error))
-    ))
-        .then(data => {
-            const restaurantList = data[0];
-            const menuList = data[1];
-            console.log(menuList);
-            getRestaurantData(restaurantList);
-        })
 
 
+const getRestaurants = () =>
+    fetch('js/restaurants.json').then(res => res.json());
+const getMenus = () => fetch('js/menu.json').then(res => res.json());
+
+
+
+const fetchData = () => Promise.all([getRestaurants(), getMenus()]);
+
+fetchData().then(([restaurants, menus]) => {
+    printRestaurants(restaurants.restaurants);
+});
+
+function printRestaurants(restaurants) {
+    const restaurantCards = restaurants.map(printRestaurantCard);
+    document.querySelector(
+        '#restaurantsContainer'
+    ).innerHTML = restaurantCards.join('');
+    attachRestaurantEvents();
 }
 
-function checkStatus(response) {
-    if (response.ok) {
-        return Promise.resolve(response);
-    } else {
-        return Promise.reject(new Error(response.statusText));
-    }
-}
-function parseJSON(response) {
-    return response.json();
-}
- function getRestaurantData(data){
-    if(data){
-        displayRestaurants(data);
-    }
- }
-
-function generateResturantCards(data){
-     let restaurantIds = [];
-    if(data) {
-        for (let i = 0; i < data.restaurants.length; i++) {
-            document.querySelector('#restaurantsContainer').innerHTML +=
-            `<div class='column-item' id='${data.restaurants[i].id}'>
+function printRestaurantCard(restaurant) {
+    return `<div class='column-item js-restaurant' id='${restaurant.id}'>
               <article class='card-item'>
-                <div class='item-content'><h4> ${data.restaurants[i].name}</h4></div>
-                    <div class='item-image'><img src='${data.restaurants[i].icon}'></div>
-                         <div class = 'item-content'><p>" ${data.restaurants[i].deal} </p> </div></article> </div>`
-            restaurantIds.push(data.restaurants[i].id);
-        }
-
-    }
- generateMenu(restaurantIds);
+                <div class='item-content'><h4> ${restaurant.name}</h4></div>
+                    <div class='item-image'><img src='${restaurant.icon}'></div>
+                         <div class = 'item-content'>
+                         <p>${restaurant.deal} </p> </div></article> </div>`;
 }
-function generateMenu (ids){
-    ids.forEach(function(data_btn){
-        data_btn.addEventListener('click', (event)=> {
-            switch (event.target.id) {
-                case "gusto":
-                    generateRestaurantMenu(data);
-                    break;
-                case "kfc":
-                    generateRestaurantMenu();
-                    break;
-            }
-         })
-    })
-}
+function printRestaurantMenu(menu){
+    return `<div class='column-item js-menu' >
+              <article class='card-item'>
+                <div class='item-content name'><h4> ${menu.denumire}</h4></div>
+                    <div class='item-image'>
+                      <img src='${menu.img}'>                       
+                    </div>
+                    <div class = 'item-content'>
+                         <p>${menu.compozitie} </p>                         
+                    </div>  
+                    <div class="item-content" style="width:fit-content">
+                      <button id='${menu.id}' class = "cart-button" data-action="ADD_TO_CART" >Adauga in cos</button></div>
+                </div>
+              </article>
+           </div> `
 
-function displayRestaurants (data){
+}
+function attachRestaurantEvents() {
+    const restaurantCards = document.querySelectorAll('.js-restaurant');
+    restaurantCards.forEach(card => {
+        card.addEventListener('click', e => {
+            fetchData().then(([restaurants, menus]) => {
+                getMenuItems(menus.menus);
+            });
+        });
+    });
+}
+document.querySelector('#comfortFood').addEventListener('click', displayItems);
+
+function displayItems(){
     document.querySelector('#kitchens').style.display = 'none';
     document.querySelector('#favorites').style.display = 'none';
     document.querySelector('#options').style.display = 'none';
-    generateResturantCards(data);
-
-}
-document.querySelector('#comfortFood').addEventListener('click',fetchData);
-
-function generateRestaurantMenu() {
-    fetchData();
-    if (data) {
-        for (let i = 0; i < data.menu.length; i++) {
-            document.querySelector('#menuContainer').innerHTML +=
-                `<table> <tr>
-                   <td>${data.menu[i].nume}</td>
-                     </tr>
-                    </table>`
-        }
-    }
+    document.querySelector('#restaurantsContainer').style.display = 'flex';
 }
 
 
+function getMenuItems(menus){
+    const menuCards = menus.map(printRestaurantMenu);
+    document.querySelector(
+        '#menuContainer'
+    ).innerHTML = menuCards.join('');
+    addToCartMenuEvents()
+
+}
+
+function addToCartMenuEvents(){
+   const buttons = document.querySelectorAll('.cart-button');
+   buttons.forEach(button => {
+        button.addEventListener('click', e => {
+           addToCart(e.currentTarget.id);
+
+        });
+    })
+
+}
+
+function addToCart(id){
+    const menus =  fetchData().then(([restaurants, menus]) => {
+        return menus.menus;
+    });
+console.log(menus);
+    menus.forEach(menu =>{ if(menu.id === id){
+        storeCartItems(menu);
+    }})
+}
+
+var cartItems = [];
+function storeCartItems (menu){
+    cartItems.push(menu);
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+}
